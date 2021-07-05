4d873e28fdcc8e413ecb0f43b75ccb5776dbfe3c
KarthickThirumalaiyandi <83874069+KarthickThirumalaiyandi@users.noreply.github.com>
2021-07-05 17:56:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
+var spg;
 var req = new XMLHttpRequest();
 console.log("hi")
 req.open('GET','https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json',true);
 req.send();
 req.onload=function(){
     var data = JSON.parse(this.response);
-  // console.log(data)
-
  
 var p = 1;
 var det = data
@@ -21,7 +19,7 @@ for (let i=1;i<11;i++)
     button.innerHTML=i
     button.addEventListener('click', () => cd(i,data));
     divi.append(button) 
-   // document.body.append(divi);
+
 }
 
     var button = document.createElement('button');
@@ -51,68 +49,145 @@ function cd(pm,data)
 {
     var tab = document.getElementById("intab")
     tab.remove()
-
-  //let rowc = divv.rows.length;
-  //console.log(rec);
   createtable(pm,data);    
 }
 
 function createtable(pg,detail)
 {
+    
     let pag 
     if (pg == 1 || pg == 'f')
     {
         pag = 0
         pk  = 9
+        spg = pg
     }
     else 
         if (pg == 2)
     {
         pag = 10
         pk  = 19
+        spg = pg
     }
     else 
          if (pg == 3)
         {
         pag = 20
         pk = 29
+        spg = pg
+
+      
         }
     else
     if(pg == 4)
         {
             pag = 30
             pk = 39
+            spg = pg
+
         }
         else if (pg == 5)
         {
             pag = 40
             pk = 49
+            spg = pg
+
         }
         else if (pg == 6)
         {
             pag = 50
             pk = 59
+            spg = pg
+
         }   
         else if (pg == 7)
         {
             pag = 60
             pk = 69
+            spg = pg
+
         }
         else if (pg == 8)
         {
             pag = 70
             pk = 79
+            spg = pg
+
         }
         else if (pg == 9)
         {
             pag = 80
             pk = 89
+            spg = pg
+
         } 
         else if (pg == 10 || pg=='l')
         {
             pag = 90
             pk = 99
+            spg = pg
+
         } 
+
+        if (spg==1 && pg=='p')
+        {
+            pag = 0
+            pk  = 9
+        }
+        else if (spg==2 && pg=='p')
+        {
+            pag = 0
+            pk  = 9
+            spg=spg-1
+        }
+        else if (spg==3 && pg=='p')
+        {
+            pag = 10
+            pk  = 19
+            spg=spg-1
+        }
+        else if (spg==4 && pg=='p')
+        {
+            pag = 20
+            pk  = 29
+            spg=spg-1
+        }
+        else if (spg==5 && pg=='p')
+        {
+            pag = 30
+            pk  = 39
+            spg=spg-1
+        }
+        else if (spg==6 && pg=='p')
+        {
+            pag = 40
+            pk  = 49
+            spg=spg-1
+        }
+        else if (spg==7 && pg=='p')
+        {
+            pag = 50
+            pk  = 59
+            spg=spg-1
+        }
+        else if (spg==8 && pg=='p')
+        {
+            pag = 60
+            pk  = 69
+            spg=spg-1
+        }
+        else if (spg==9 && pg=='p')
+        {
+            pag = 70
+            pk  = 79
+            spg=spg-1
+        }
+        else if (spg==10 && pg=='p')
+        {
+            pag = 80
+            pk  = 89
+            spg=spg-1
+        }
    
 var div1 = document.createElement('div')
 div1.setAttribute("id","intab")
@@ -122,7 +197,6 @@ table.setAttribute("id","tab2")
 table.className="tab1";
 
 var thead = document.createElement('thead');
-//thead.className='tdw';s
 var tr=document.createElement('tr');
 var th1= createthtd('th','ID')
 var th2= createthtd('th','Name')
@@ -149,9 +223,7 @@ for(let j=pag;j<=pk;j++)
     table.append(thead,tbody)
 
 }
-spg=pg;
-pag =0;
-pk =0;
+
 div1.append(table)
 document.body.append(div1)
 
@@ -162,6 +234,3 @@ function createthtd(ele,value=' ')
     return td
 }
 }
-   
-
-
